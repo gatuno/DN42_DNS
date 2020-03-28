6e39f4cdfb7d1ee3350e91de301b802e95cc9b03
Maraun <maraun@web.de>
2020-03-28 18:55:22 +0100

    DNS state progressbars

--- src/DNS42/Views/Index.php
+++ src/DNS42/Views/Index.php
@@ -5,7 +5,18 @@ Gatuf::loadFunction('Gatuf_Shortcuts_RenderToResponse');
 
 class DNS42_Views_Index {
 	public function index ($request, $match) {
-		return Gatuf_Shortcuts_RenderToResponse ('dns42/index.html', array ('page_title' => __('Start page')), $request);
+		$values = array( 'page_title' => __('Start page'),
+			'nsip4' => '20',
+			'nsip6'	=> '8',
+			'nsip46' => '70',
+			'onip4' => 10,
+			'offip4' => 90,
+			'onip6'	=> 8,
+			'offip6'=> 92,
+			'onip46' => 20,
+			'offip46' => 80);
+
+		return Gatuf_Shortcuts_RenderToResponse ('dns42/index.html', $values, $request);
 	}
 }
 
